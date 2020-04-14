51ddd4464bb7d7e515c1617510a8d714e4fe0ebc
LOUNIS AMAR <56897657+LounisAmarLaZeroDay@users.noreply.github.com>
2020-04-14 18:46:54 +0100

    Signed-off-by: LOUNIS AMAR <56897657+LounisAmarLaZeroDay@users.noreply.github.com>

---
+++ precache-manifest.7c1a3a5d1cdb7f9e3d6ec3d21ccca44e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1059085248752ab28f11335499fdf4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73a2b3ddf7243b8a0f39",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f2927626204d41e8ff0",
+    "url": "/static/js/2.84de93d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84de93d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73a2b3ddf7243b8a0f39",
+    "url": "/static/js/main.55bed9fc.chunk.js"
+  },
+  {
+    "revision": "3d9885d62ced1fe23186",
+    "url": "/static/js/runtime-main.91f4cc95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
